f48f482714930c2f2834b1cb3a0885ad15c833ee
dheerajy <44141999+dheerajy1@users.noreply.github.com>
2022-10-30 18:27:12 +0530

    update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const timeleft = document.getElementById("time-left");
 
-const birthday = new Date("11/25/2022");
-
+const birthday = new Date("12/25/2022");
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
@@ -10,16 +9,21 @@ let timerId;
 function countdown() {
   const today = new Date();
   const timespan = birthday - today;
-  if (timespan <= -day) {
-    timeleft.innerHTML = "Hope you had a great day!";
+  if(timespan<=-day){
+    timeleft.innerHTML = "Hope you had a nice Birthday!";
     clearInterval(timerId);
     return;
   }
-  if (timespan <= 0) {
+  else if (timespan <= 0) {
     timeleft.innerHTML = " Happy Birthday";
     clearInterval(timerId);
     return;
   }
+const days = Math.floor(timespan/day);
+const hours = Math.floor((timespan%day)/hour);
+const minutes =Math.floor((timespan%hour)/minute);
+const seconds = Math.floor((timespan%minute)/second);
+timeleft.innerHTML= days+'days '+hours+'hours '+minutes+'minutes '+seconds+'seconds';
 }
 
 timerId = setInterval(countdown, second);
